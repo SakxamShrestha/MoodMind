0a8f0f97ded071eb325bb876ea0a4079d454619a
Sakxam Shrestha <143928022+SakxamShrestha@users.noreply.github.com>
2024-10-28 03:27:47 -0500

    Updating utils

--- utils/db.ts
+++ utils/db.ts
@@ -1,13 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
+// Defining a global type for the Prisma instance, allowing it to persist across module reloads in development
 const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined
-  }
-  
-  export const prisma =
-    globalForPrisma.prisma ??
-    new PrismaClient({
-      log: ['query'],
-    })
-  
-  if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+  prisma: PrismaClient | undefined;
+};
+
+// Exporting the Prisma instance, checking if one already existed in `globalForPrisma`.
+// This is preventing the creation of multiple instances during hot-reloading in development.
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    // Logging all queries to the console for debugging purposes
+    log: ['query'],
+  });
+
+// In non-production environments, storing the Prisma instance globally to avoid duplication
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
