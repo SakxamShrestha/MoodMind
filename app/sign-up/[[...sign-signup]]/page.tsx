6a2c76f04c72c5c771f91f3860991edf78e0947f
Sakxam Shrestha <143928022+SakxamShrestha@users.noreply.github.com>
2024-10-28 03:56:51 -0500

    Updated smth

--- app/sign-up/[[...sign-signup]]/page.tsx
+++ app/sign-up/[[...sign-signup]]/page.tsx
@@ -1,6 +1,6 @@
 import {SignUp} from "@clerk/nextjs";
 const SignUpPage = () => {
-    return <SignUp afterSignOutUrl="/new-user" redirectUrl="/new-user"/>
+    return <SignUp afterSignOutUrl="/new-user" forceRedirectUrl="/new-user"/>
 
 }
 
