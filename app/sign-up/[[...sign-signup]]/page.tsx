ec40a71649cf57662b69e18a9149003e2b23fa8b
Sakxam Shrestha <143928022+SakxamShrestha@users.noreply.github.com>
2024-11-04 19:20:00 -0600

    Changed middleware code

--- app/sign-up/[[...sign-signup]]/page.tsx
+++ app/sign-up/[[...sign-signup]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from '@clerk/nextjs';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function SignUpPage() {
